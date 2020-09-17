54898411d55c179fb9a724bbf7add46a70040f5e
lauphern <lau.p.hern@hotmail.com>
2020-09-17 11:44:26 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace trump_quotes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
