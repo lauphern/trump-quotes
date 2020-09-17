632d527410fb624ba81299d0876efda21c26d2f1
lauphern <lau.p.hern@hotmail.com>
2020-09-17 17:56:52 +0200

    now calling HttpQuoteService.ConnectApi (former RunAsync) in Program.cs because there were two Main entry points

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TrumpQuotes.WebSite.Services;
 
 namespace TrumpQuotes
 {
@@ -14,6 +15,7 @@ namespace TrumpQuotes
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            HttpQuoteService.ConnectApi();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
