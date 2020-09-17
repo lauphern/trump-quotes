e6ecc971766f37fbafff847a7e43ebb733f4b490
lauphern <lau.p.hern@hotmail.com>
2020-09-17 13:58:51 +0200

    created link model

---
+++ Models/LinkModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TrumpQuotes.Models
+{
+  public class Link
+  {
+    public Self Self { get; set; }
+  }
+
+}
