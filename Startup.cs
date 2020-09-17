0c9a49155f3f1b52efce7d7df48ad2293a1421f0
lauphern <lau.p.hern@hotmail.com>
2020-09-17 16:20:48 +0200

    added HttpQuoteService to Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TrumpQuotes.WebSite.Services;
 
 namespace TrumpQuotes
 {
@@ -24,6 +25,7 @@ namespace TrumpQuotes
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<HttQuoteService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
