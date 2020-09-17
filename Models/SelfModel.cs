85d6a7c7fd0f1d220fc2d9bd1a56cd1b52f52ffb
lauphern <lau.p.hern@hotmail.com>
2020-09-17 13:59:53 +0200

    created self model

---
+++ Models/SelfModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TrumpQuotes.Models
+{
+  public class Self
+  {
+    public string Href { get; set; }
+  }
+
+}
